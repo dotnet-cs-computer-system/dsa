7c8b03b9e9b09986a5c9a5fee9287d01fe2ee6ca
dnquangit <dnquangit@gmail.com>
2025-11-20 13:40:41 +0700

    Add binary search

---
+++ correct-binary-search/BinarySearch.Core/BinarySearchLibrary.cs
@@ -0,0 +1,29 @@
+﻿namespace BinarySearch.Core;
+
+public class BinarySearchLibrary
+{
+    public static int Search(int[] array, int n)
+    {
+        // low, high, loop
+        int low = 0;
+        int high = array.Length - 1;
+        while (low <= high)
+        {
+            int mid = (low + high) / 2;
+            
+            if (n == array[mid])
+            {
+                return mid;
+            }
+            else if (n < array[mid])
+            {   
+                high = mid - 1;
+            }
+            else
+            {
+                low = mid + 1;
+            }
+        }
+        return -1;
+    }
+}
---
+++ correct-binary-search/BinarySearch.Tests.Unit/BinarySearchLibraryTests.cs
@@ -0,0 +1,22 @@
+﻿using BinarySearch.Core;
+using FluentAssertions;
+
+namespace BinarySearch.Tests.Unit;
+
+public class BinarySearchLibraryTests
+{
+    [Fact]
+    public void Search_ReturnCorrectValue_WhenArrayIsProvided()
+    {
+        // arrange
+        int[] array = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+        int n = 4;
+        int expectedIndex = 3;
+        
+        // action 
+        int result = BinarySearchLibrary.Search(array, n);
+
+        // assert 
+        result.Should().Be(expectedIndex);
+    }
+}
